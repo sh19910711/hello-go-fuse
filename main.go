2cdf6187c700515b458c78632574fc74521800d9
Hiroyuki Sano <sh19910711@gmail.com>
2016-03-31 11:41:19 +0900

    hello-fs: add main.go

---
+++ main.go
@@ -0,0 +1,62 @@
+// ref: https://github.com/hanwen/go-fuse/blob/e73817414197884414aad76cf99983fa442ee3ce/example/hello/main.go
+package main
+
+import(
+  "flag"
+  "log"
+
+  "github.com/hanwen/go-fuse/fuse"
+  "github.com/hanwen/go-fuse/fuse/nodefs"
+  "github.com/hanwen/go-fuse/fuse/pathfs"
+)
+
+type HelloFs struct {
+  pathfs.FileSystem
+}
+
+func (me *HelloFs) GetAttr(name string, context *fuse.Context) (*fuse.Attr, fuse.Status) {
+  switch name {
+    case "hello.txt":
+      return &fuse.Attr {
+        Mode: fuse.S_IFREG | 0644, Size: uint64(len(name)),
+      }, fuse.OK
+
+    case "":
+      return &fuse.Attr {
+        Mode: fuse.S_IFDIR | 0755,
+      }, fuse.OK
+
+  }
+  return nil, fuse.ENOENT
+}
+
+func (me *HelloFs) OpenDir(name string, context *fuse.Context) (c []fuse.DirEntry, code fuse.Status) {
+  if name == "" {
+    c = []fuse.DirEntry{{Name: "hello.txt", Mode: fuse.S_IFREG}}
+    return c, fuse.OK
+  }
+  return nil, fuse.ENOENT
+}
+
+func (me *HelloFs) Open(name string, flags uint32, context *fuse.Context) (file nodefs.File, code fuse.Status) {
+  if name != "hello.txt" {
+    return nil, fuse.ENOENT
+  }
+  if flags & fuse.O_ANYWRITE != 0 {
+    return nil, fuse.EPERM
+  }
+  return nodefs.NewDataFile([]byte(name)), fuse.OK
+}
+
+func main() {
+  flag.Parse()
+  if len(flag.Args()) < 1 {
+    log.Fatal("Usage: hello <mnt/>")
+  }
+  nfs := pathfs.NewPathNodeFs(&HelloFs{FileSystem: pathfs.NewDefaultFileSystem()}, nil)
+  server, _, err := nodefs.MountRoot(flag.Arg(0), nfs.Root(), nil)
+  if err != nil {
+    log.Fatal(err)
+  }
+  server.Serve()
+}
